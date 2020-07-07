54784c04c2a3c04c9faeb6487a5e5c2bf35f83df
SnyderMbishai <csmbishai@gmail.com>
2020-07-07 16:33:25 +0300

    Add rest framework

--- hrt/settings.py
+++ hrt/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'hello',
     'todo',
     'vowel_reverser',
@@ -74,6 +75,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'hrt.wsgi.application'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Database
