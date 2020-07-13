4c24015fac2771ed5429173936d7fe846c4dbe8b
SnyderMbishai <csmbishai@gmail.com>
2020-07-13 12:16:21 +0300

    Override create method

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,12 +1,19 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from .models import ToDo, ToDoItems
-
-class ToDoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ToDo
-        fields = "__all__"
 
 class ToDoItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDoItems
-        fields = "__all__"
+        fields = ("id","title","description", "created_at")
+        read_only_fields = ("todo",)
+
+    def create(self,validated_data):
+        todo = get_object_or_404(ToDo, pk=self.context["view"].kwargs["todo_pk"])
+        return ToDoItems.objects.create(**validated_data, todo=todo)
+
+class ToDoSerializer(serializers.ModelSerializer):
+    todo_items = ToDoItemsSerializer(many=True, read_only=True)
+    class Meta:
+        model = ToDo
+        fields = ("title","description", "todo_items")
