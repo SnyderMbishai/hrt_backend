80e9abce74de3f43803ce58e5d12d41b2dee4223
SnyderMbishai <csmbishai@gmail.com>
2020-07-13 12:16:39 +0300

    Add views for todo

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions, generics, viewsets
+
+from .serializers import ToDoSerializer, ToDoItemsSerializer
+from .models import ToDo, ToDoItems
+
+class ToDoView(generics.ListCreateAPIView):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
+
+class ToDoDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
+
+class ToDoItemsView(generics.ListCreateAPIView):
+    serializer_class = ToDoItemsSerializer
+    queryset = ToDoItems.objects.all()
+
+class ToDoItemsDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ToDoItemsSerializer
+    queryset = ToDoItems.objects.all()
+    
