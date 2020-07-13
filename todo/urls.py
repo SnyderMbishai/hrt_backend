c7cfc3086b5aefd98137df29de24e17b99f56584
SnyderMbishai <csmbishai@gmail.com>
2020-07-13 12:16:54 +0300

    Add todo urls

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'todo'
 
 urlpatterns = [
-     # path('create')
+     path('todos', views.ToDoView.as_view(), name="todos"),
+     path('todos/<int:pk>', views.ToDoDetailsView.as_view(), name="todo-details"),
+
+     path('todos/<int:todo_pk>/todo-items', views.ToDoItemsView.as_view(), name="tod0-items" ),
+     path('todos/<int:todo_pk>/todo-items/<int:pk>',views.ToDoItemsDetailsView.as_view(), name="todo-item-details"),
 ]
