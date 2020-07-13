1190dafa36678fb34ad7474d3e440209ced981ff
SnyderMbishai <csmbishai@gmail.com>
2020-07-13 12:15:28 +0300

    Auto add dates

--- todo/models.py
+++ todo/models.py
@@ -1,18 +1,19 @@
+import datetime
 from django.db import models
 
 # Create your models here
 
 class ToDo(models.Model):
-    title = models.CharField(max_length=100, blank=False)
-    description = models.CharField(max_length=200)
-    created_at = models.DateTimeField(blank=False)
-    edited_at = models.DateTimeField(blank=True)
+    title = models.CharField(max_length=128, blank=False)
+    description = models.CharField(blank=True, max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    edited_at = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
 
 class ToDoItems(models.Model):
-    todo_list = models.ForeignKey(ToDo, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, blank=False)
-    description = models.CharField(max_length=200)
-    created_at = models.DateTimeField(blank=False)
-    edited_at = models.DateTimeField(blank=True)
-    is_completed = models.BooleanField(default=False)
+    description = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)    
+    todo = models.ForeignKey(ToDo, related_name="todo_items", on_delete=models.CASCADE)
